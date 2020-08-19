e125dfa4929f01ea93ff79c7e059fbf04d4c85e7
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-19 10:21:33 +0200

    Verified by Cia V1.0.3

--- index.js
+++ index.js
@@ -1,54 +1,150 @@
-// Catch the ID
 const cvs = document.getElementById("canvas");
-// Using getContext method
-const ctx = cvs.getContext('2d');
+const ctx = cvs.getContext("2d");
 
-//Create unit
+// create the unit
+// every unit has 32 pixels!
 const box = 32;
 
-
-//Load Images
+// load images
 const ground = new Image();
-ground.src   = "./img/ground.png";
+ground.src = "img/ground.png";
 
-const foodIMG = new Image();
-foodIMG.src   = "./img/food.png";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
+// load audio files
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
 
-//Load Audio files!
-const dead  = new Audio();
-const eat   = new Audio();
-const up    = new Audio();
-const down  = new Audio();
-const left  = new Audio();
-const right = new Audio();
-//Import source file!
-dead.src   = "./audio/dead.mp3";
-eat.src    = "./audio/eat.mp3";
-up.src     = "./audio/up.mp3";
-down.src   = "./audio/down.mp3";
-left.src   = "./audio/left.mp3";
-right.src  = "./audio/right.mp3";
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
 
+// create the snake
 
-//Create the snake
-let snake = []
+let snake = [];
 
 snake[0] = {
-    x: 9 * box,
-    y: 10 * box,
-} 
+    x : 9 * box,
+    y : 10 * box
+};
 
-//Create the food
+// create the food
+// x is left to right & right to left ! 
+// y is up to down & down to up ! 
 let food = {
-    x: Math.floor(Math.random()* 17+1) * box,
-    y: Math.random(Math.random()*15+3) * box,
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
 }
 
-//Create the score variable
+// create the score var
 let score = 0;
 
-//Control the Snake!
+//control the snake
 let d;
 
-document.addEventListener("keydown", direction)
+document.addEventListener("keydown",direction);
+// this is how to define keyboard keys 
+// which is like this => 
+//37 is the Left key (A)
+//38 is the Up key (W)
+// 39 is the right key (D)
+// 40 is the Bottom(Down) key (S)
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        left.play();
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+        up.play();
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+        right.play();
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+        down.play();
+    }
+}
+
+// cheack collision function
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// draw everything to the canvas
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "green" : "white";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // which direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // if the snake eats the food
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        eat.play();
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // we don't remove the tail
+    }else{
+        // remove the tail
+        snake.pop();
+    }
+    
+    // add new Head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // game over
+    // this is a function which define game over logic
+    // this means if you hit the walls or snake's tail you're K.O ! :)
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+// call draw function every 100 ms
+
+let game = setInterval(draw,100);
